35720a6ccef8863cacd0bcc418ce80a8958bf5d6
tissad <tissad@student.42.fr>
2026-02-17 16:58:52 +0100

    add header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,39 +6,25 @@
 /*   By: tissad <tissad@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/16 14:12:22 by tissad            #+#    #+#             */
-/*   Updated: 2026/02/16 18:42:20 by tissad           ###   ########.fr       */
+/*   Updated: 2026/02/17 16:58:17 by tissad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 'use client'
 
+
 import { toggleDarkMode } from './darkMode'
+import Header from '../components/Header' 
 
 
 
 
 export default function Home() {
   return (
+    
     <main className="min-h-screen bg-gray-50 dark:bg-background transition-colors duration-300">
-      test dark mode
-      <h1 className="text-danger/20"> Hello, World!</h1>
-      <h1> Hello, World!</h1>
-      <div className="bg-primary/10 p-4 rounded-base bg-color-surface">
-        <p className="text-primary">This is a card component.</p>
-        <p className="text-foreground">It uses Tailwind CSS for styling.</p>
-      </div>
-      <p className="text-primary">Welcome to my portfolio website.</p>
-      <p className="text-foreground">Feel free to explore my projects and skills.</p>
-      
-      <h1 className="text-primary dark:text-danger">Hello, World!</h1>
-      <p className="text-primary dark:text-danger">
-        Ce texte change de couleur en dark mode.
-      </p>
-      <div className="dark:text-primary dark:bg-foreground  p-4 rounded">
-        Carte avec fond différent en dark mode
-      </div>
-      
-      
+      <Header />
+          
       <button className="bg-primary text-foreground p-2 rounded hover:bg-primary/80 transition-colors" onClick={() => toggleDarkMode()}>
           Toggle Dark Mode
       </button>
---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Header.tsx                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tissad <tissad@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/17 16:57:18 by tissad            #+#    #+#             */
+/*   Updated: 2026/02/17 16:57:29 by tissad           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+export default function Header() {
+    return (
+        <header className="bg-primary text-foreground p-4">
+        <h1 className="text-2xl font-bold">Tahar Issad - Portfolio</h1>
+        </header>
+    )
+}   
